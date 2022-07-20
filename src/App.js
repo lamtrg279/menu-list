1f58fadf2547a350b5508ff866474077623f5fc4
Lam Truong <nl2791999@gmail.com>
2022-07-20 14:03:23 -0500

    Styling

--- src/App.js
+++ src/App.js
@@ -10,9 +10,34 @@ const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [activeCategory, setActiveCategory] = useState("");
-  const [categories, setCategories] = useState();
+  const [categories, setCategories] = useState(allCategories);
 
-  return <div className='App'></div>;
+  const filterItems = (category) => {
+    setActiveCategory(category);
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <img src={logo} alt='logo' className='logo' />
+          <h2>Menu List</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories
+          categories={categories}
+          activeCategory={activeCategory}
+          filterItems={filterItems}
+        />
+      </section>
+    </main>
+  );
 }
 
 export default App;
