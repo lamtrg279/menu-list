3b951a359d580d41794c0092bd220026d8e78709
Lam Truong <nl2791999@gmail.com>
2022-07-20 13:25:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Menu from "./menu";
+import Categories from "./categories";
+import items from "./data";
+import logo from "./logo.png";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [menuItems, setMenuItems] = useState(items);
+  const [activeCategory, setActiveCategory] = useState("");
+  const [categories, setCategories] = useState();
+
+  return <div className='App'></div>;
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,86 @@
+const menu = [
+  {
+    id: 1,
+    title: "vietnamese eggroll",
+    category: "appetizer",
+    price: 6.95,
+    img: "./images/item1.jpg",
+    description:
+      "These Vietnamese egg rolls crispy, deep fried, snacks made of pork, mung bean thread, jicama, onion, and mushroom"
+  },
+  {
+    id: 2,
+    title: "vietnamse springroll",
+    category: "appetizer",
+    price: 6.5,
+    img: "/.images/item2.jpg",
+    description:
+      "An authentic, delicious, and relatively easy Vietnamese recipe of spring rolls filled with pork, shrimp, rice noodles and veggies"
+  },
+  {
+    id: 3,
+    title: "pork chop rice platter",
+    category: "rice",
+    price: 15.95,
+    img: "/.images/item3.jpg",
+    description:
+      "Broken rice dish including grilled ribs (pork ribs are marinated with sweet and sour spices then grill), rolls (made from eggs, minced meat), omelet, pork skin (sliced ​​pork skin mixed with spices), Chinese sausage and pork meat"
+  },
+  {
+    id: 4,
+    title: "special pho",
+    category: "pho",
+    price: 15.95,
+    img: "/.images/item4.jpg",
+    description:
+      "Broken rice dish including grilled ribs (pork ribs are marinated with sweet and sour spices then grill), rolls (made from eggs, minced meat), omelet, pork skin (sliced ​​pork skin mixed with spices), Chinese sausage and pork meat"
+  },
+
+  {
+    id: 5,
+    title: "Vermicelli Noodle",
+    category: "bun",
+    price: 15.95,
+    img: "/.images/item5.jpg",
+    description:
+      "Vermicelli noodles, cucumber, bean sprouts, herbs, grilled shrimp, and a sweet-and-sour sauce all feature in this very delicious Vietnamese salad"
+  },
+  {
+    id: 6,
+    title: "bun bo hue",
+    category: "bun",
+    price: 15.95,
+    img: "/.images/item6.jpg",
+    description:
+      "Bun bo Hue or bun bo is a popular Vietnamese soup containing rice vermicelli and beef"
+  },
+  {
+    id: 7,
+    title: "vietnamese fried rice",
+    category: "rice",
+    price: 15.95,
+    img: "/.images/item7.jpg",
+    description:
+      "A dish of boiled or steamed rice is stir-fried with soy sauce, beaten egg, chopped meat, and vegetables"
+  },
+  {
+    id: 8,
+    title: "banh mi",
+    category: "sandwich",
+    price: 8.95,
+    img: "/.images/item8.jpg",
+    description:
+      "Crusty bread rolls smeared with pate, mayo, suspicious looking Asian ham, pickled vegetables, green onion, coriander/cilantro, a mighty wack of fresh chillies and drizzle of seasoning"
+  },
+  {
+    id: 0,
+    title: "vietnamese coffee",
+    category: "drink",
+    price: 6.95,
+    img: "/.images/item9.jpg",
+    description:
+      "Rich, flavorful, and sweet coffee brewed from Vietnamese coffee beans using a special filter with sweet condensed milk"
+  }
+];
+
+export default menu;
